f7625d8aab34422b9e395e1c4122989fb7b86492
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-04-06 14:41:24 -0700

    cleaned up comments

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const inquirer = require("inquirer");
 require("console.table");
 const db = require("./db/connection");
 
-// WHEN I start the application
-//THEN I am presented with the following OPTIONS: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+// start the command line application, and connect to the database with options to view view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 const prompt = inquirer.createPromptModule();
 function runOptions() {
   prompt([
@@ -49,8 +48,7 @@ function runOptions() {
   });
 }
 
-// WHEN I choose to VIEW all departments
-// THEN I am presented with a formatted table showing department names and department ids
+// view all current departments in the database
 function viewDepartments() {
   db.promise()
     .query("SELECT * FROM department")
@@ -60,8 +58,7 @@ function viewDepartments() {
     });
 }
 
-// WHEN I choose to VIEW all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// view all current employee roles in the database
 function viewRoles() {
   db.query("SELECT * FROM role", function (err, results) {
     console.table(results);
@@ -69,8 +66,7 @@ function viewRoles() {
   });
 }
 
-// WHEN I choose to VIEW all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// view all current employees in the database, including their roleID and managerID
 function viewEmployee() {
   db.query("SELECT * FROM employee", function (err, results) {
     console.table(results);
@@ -78,8 +74,7 @@ function viewEmployee() {
   });
 }
 
-// WHEN I choose to ADD a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
+//add a department name to the database
 function addDepartment() {
   // inquirer prompt to get the department name
   inquirer
@@ -103,8 +98,7 @@ function addDepartment() {
     });
 }
 
-// WHEN I choose to ADD a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// add employee role to the database
 function addRole() {
   inquirer
     .prompt([
@@ -137,8 +131,7 @@ function addRole() {
     });
 }
 
-// WHEN I choose to ADD an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// add employee to the database
 function addEmployee() {
   inquirer
     .prompt([
@@ -185,8 +178,7 @@ function addEmployee() {
     });
 }
 
-// WHEN I choose to UPDATE an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+// update employee role ID in the database
 function updateEmployee() {
   inquirer
     .prompt([
